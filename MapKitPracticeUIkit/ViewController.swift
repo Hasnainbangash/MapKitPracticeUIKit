44cdc385ff1148298f028268773e48a7e84a07f1
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-01-02 14:04:56 +0500

    Code for making direction from one point to another manually

--- MapKitPracticeUIkit/ViewController.swift
+++ MapKitPracticeUIkit/ViewController.swift
@@ -34,6 +34,8 @@ class ViewController: UIViewController {
             setInitialMapLocations()
         }
         
+        routeMap()
+        
     }
     
     func setupProfileImage() {
@@ -74,6 +76,44 @@ class ViewController: UIViewController {
 
 extension ViewController: MKMapViewDelegate {
     
+    func showRouteOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D) {
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: pickupCoordinate, addressDictionary: nil))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: destinationCoordinate, addressDictionary: nil))
+        request.requestsAlternateRoutes = true
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
+            
+            //for getting just one route
+            if let route = unwrappedResponse.routes.first {
+                //show on map
+                self.mapView.addOverlay(route.polyline)
+                //set the map area to show the route
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets.init(top: 80.0, left: 20.0, bottom: 100.0, right: 20.0), animated: true)
+            }
+            
+            //if you want to show multiple routes then you can get all routes in a loop in the following statement
+            //for route in unwrappedResponse.routes {}
+        }
+    }
+    
+    func routeMap() {
+        //create two dummy locations
+        let loc1 = CLLocationCoordinate2D.init(latitude: 51.513059, longitude: -0.091404)
+        let loc2 = CLLocationCoordinate2D.init(latitude: 51.516925, longitude: -0.089387)
+
+//        51.513059, -0.091404
+//        51.516925, -0.089387
+        
+        //find route
+        showRouteOnMap(pickupCoordinate: loc1, destinationCoordinate: loc2)
+    }
+    
     func addRadiusCircle(location: CLLocation){
         self.mapView.delegate = self
         let circle = MKCircle(center: location.coordinate, radius: 500 as CLLocationDistance)
@@ -89,7 +129,10 @@ extension ViewController: MKMapViewDelegate {
             return circle
         }
         
-        return MKOverlayRenderer()
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.red
+        renderer.lineWidth = 5.0
+        return renderer
     }
     
 }
